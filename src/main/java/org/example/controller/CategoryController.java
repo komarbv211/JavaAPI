2c94d87c917cdd20e02a1250dadb5aba2151f99f
komarbv211 <andrijkoslak@gmail.com>
2025-02-12 19:46:26 +0200

    add project

---
+++ src/main/java/org/example/controller/CategoryController.java
@@ -0,0 +1,38 @@
+package org.example.controller;
+
+import org.example.dto.category.CategoryCreateDTO;
+import org.example.dto.category.CategoryEditDTO;
+import org.example.entites.CategoryEntity;
+import org.example.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public List<CategoryEntity> getAllCategories() {
+        return categoryService.getList();
+    }
+
+    @PostMapping
+    public CategoryEntity create(CategoryCreateDTO dto) {
+        return categoryService.create(dto);
+    }
+
+    @PutMapping
+    public CategoryEntity edit(CategoryEditDTO dto) {
+        return categoryService.edit(dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id) {
+        categoryService.delete(id);
+    }
+}
