59e2b3fc2e8ea336afcd54dd912c5ce1d61a0cdc
komarbv211 <andrijkoslak@gmail.com>
2025-03-04 19:57:45 +0200

    Improvements in updateProduct

--- src/main/java/org/example/repository/IProductImageRepository.java
+++ src/main/java/org/example/repository/IProductImageRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface IProductImageRepository extends JpaRepository<ProductImageEntity,Integer> {
     Optional<ProductImageEntity> findByName(String name);
+
 }
--- src/main/java/org/example/service/ProductService.java
+++ src/main/java/org/example/service/ProductService.java
@@ -3,7 +3,6 @@ package org.example.service;
 import lombok.AllArgsConstructor;
 import org.example.dto.product.ProductItemDTO;
 import org.example.dto.product.ProductPostDTO;
-import org.example.dto.product.ProductPutDTO;
 import org.example.entites.CategoryEntity;
 import org.example.entites.ProductEntity;
 import org.example.entites.ProductImageEntity;
@@ -11,8 +10,12 @@ import org.example.mapper.ProductMapper;
 import org.example.repository.IProductImageRepository;
 import org.example.repository.IProductRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -57,39 +60,69 @@ public class ProductService {
         return entity;
     }
 
-    public boolean updateProduct(Integer id, ProductPutDTO product) {
-        var res = productRepository.findById(id);
-        if (res.isEmpty()) {
-            return false;
-        }
-        var entity = res.get();
+    public boolean updateProduct(Integer id, ProductPostDTO product) {
+        var entity = productRepository.findById(id).orElseThrow();
+
+        // Оновлення основних даних продукту
         entity.setName(product.getName());
         entity.setDescription(product.getDescription());
         entity.setPrice(product.getPrice());
+
         var cat = new CategoryEntity();
         cat.setId(product.getCategoryId());
         entity.setCategory(cat);
-        productRepository.save(entity);
 
-        for (var img : product.getRemoveImages()) {
-            var removeImage = productImageRepository.findByName(img).get();
-            fileService.remove(img);
-            productImageRepository.delete(removeImage);
-        }
+        List<String> oldImageNames = product.getImages().stream()
+                .filter(img -> "old-image".equals(img.getContentType()))
+                .map(MultipartFile::getOriginalFilename)
+                .toList();
 
+        List<ProductImageEntity> updatedImages = new ArrayList<>();
         int priority = 1;
-        for (var img : product.getImages()) {
-            ProductImageEntity image = new ProductImageEntity();
-            var imageName = fileService.load(img);
-            image.setName(imageName);
-            image.setPriority(priority);
-            image.setProduct(entity);
-            productImageRepository.save(image);
-            priority++;
+
+        // Видалення фото, яких більше немає у списку
+        var imagesToRemove = entity.getImages().stream()
+                .filter(img -> !oldImageNames.contains(img.getName()))
+                .toList();
+
+        for (var img : imagesToRemove) {
+            fileService.remove(img.getName());
+            productImageRepository.delete(img);
         }
 
+        // Оновлення пріоритетів старих фото
+        for (var img : entity.getImages()) {
+            if (oldImageNames.contains(img.getName())) {
+                int index = oldImageNames.indexOf(img.getName());
+                if (index != -1) {
+                    img.setPriority(index);
+                    updatedImages.add(img);
+                }
+            }
+        }
+
+        // Додавання нових фото
+        for (var img : product.getImages()) {
+            if (!"old-image".equals(img.getContentType())) {
+                var imageName = fileService.load(img);
+                var newImage = new ProductImageEntity();
+                newImage.setName(imageName);
+                int newPriority = product.getImages().indexOf(img);
+                if (newPriority != -1) {
+                    newImage.setPriority(newPriority);
+                }
+                newImage.setProduct(entity);
+                updatedImages.add(newImage);
+            }
+        }
+
+        // Збереження оновлених фото
+        productImageRepository.saveAll(updatedImages);
+        productRepository.save(entity);
         return true;
     }
+
+
 
     public boolean deleteProduct(Integer id) {
         var res = productRepository.findById(id);
